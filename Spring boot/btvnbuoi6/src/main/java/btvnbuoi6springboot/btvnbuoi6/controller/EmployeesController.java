6ebdbab8639f0881ed4d6add93e16f562c95a2e4
thachpham2103 <phamthach2103@gmail.com>
2025-05-01 23:54:15 +0700

    employees

--- Spring boot/btvnbuoi6/src/main/java/btvnbuoi6springboot/btvnbuoi6/controller/EmployeesController.java
+++ Spring boot/btvnbuoi6/src/main/java/btvnbuoi6springboot/btvnbuoi6/controller/EmployeesController.java
@@ -1,10 +1,14 @@
 package btvnbuoi6springboot.btvnbuoi6.controller;
 
+import btvnbuoi6springboot.btvnbuoi6.dto.employeesDTO.EmployeesCreateRequest;
+import btvnbuoi6springboot.btvnbuoi6.dto.employeesDTO.EmployeesUpdateRequest;
+import btvnbuoi6springboot.btvnbuoi6.entity.Employees;
 import btvnbuoi6springboot.btvnbuoi6.service.EmployeesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ecl")
@@ -13,5 +17,25 @@ public class EmployeesController {
   @Autowired
     private EmployeesService employeesService;
 
+  @GetMapping("/emp")
+  List<Employees> getEmployees(@RequestParam long id){
+    return employeesService.getEmployees(id);
+  }
+
+  @PostMapping("/emp")
+  Employees createEmployees(@RequestBody EmployeesCreateRequest request){
+    return employeesService.createEmployees(request);
+  }
+
+  @PutMapping("/{id}")
+  public Employees updateEmployees(@PathVariable long id, @RequestBody EmployeesUpdateRequest request){
+    return employeesService.updateEmployees(request, id);
+  }
+
+  @DeleteMapping("/{id}")
+  String deleteEmployees(@PathVariable long id){
+    employeesService.deleteDepartment(id);
+    return "thành công!!!";
+  }
 
 }
--- Spring boot/btvnbuoi6/src/main/java/btvnbuoi6springboot/btvnbuoi6/service/EmployeesService.java
+++ Spring boot/btvnbuoi6/src/main/java/btvnbuoi6springboot/btvnbuoi6/service/EmployeesService.java
@@ -1,9 +1,14 @@
 package btvnbuoi6springboot.btvnbuoi6.service;
 
 
+import btvnbuoi6springboot.btvnbuoi6.dto.employeesDTO.EmployeesCreateRequest;
+import btvnbuoi6springboot.btvnbuoi6.dto.employeesDTO.EmployeesUpdateRequest;
+import btvnbuoi6springboot.btvnbuoi6.entity.Employees;
 import btvnbuoi6springboot.btvnbuoi6.repository.EmployeesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeesService {
@@ -11,5 +16,37 @@ public class EmployeesService {
   @Autowired
     private EmployeesRepository employeesRepository;
 
+  public List<Employees> getEmployees(long id){
+    return employeesRepository.findAll();
+  }
 
+  public Employees createEmployees(EmployeesCreateRequest request){
+
+    Employees employees= new Employees();
+
+    employees.setFullName(request.getFullName());
+    employees.setBirthday(request.getBirthday());
+    employees.setHiredDate(request.getHiredDate());
+    employees.setSalary(request.getSalary());
+
+    return employeesRepository.save(employees);
+  }
+
+  public Employees updateEmployees(EmployeesUpdateRequest request, long id){
+
+    Employees employees= (Employees) getEmployees(id);
+    employees.setFullName(request.getFullName());
+    employees.setBirthday(request.getBirthday());
+    employees.setHiredDate(request.getHiredDate());
+    employees.setSalary(request.getSalary());
+
+    return employeesRepository.save(employees);
+
+  }
+
+  public void deleteDepartment(long id){
+    employeesRepository.deleteById(id);
+  }
+
+  
 }
