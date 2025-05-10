59ac1c88f9ea61c3dba9aa6172948de66a0b00b8
thachpham2103 <phamthach2103@gmail.com>
2025-05-11 00:30:44 +0700

    update

--- Spring boot/baikt/src/main/java/com/example/buoi7/baikt/controller/BookController.java
+++ Spring boot/baikt/src/main/java/com/example/buoi7/baikt/controller/BookController.java
@@ -1,15 +1,15 @@
 package com.example.buoi7.baikt.controller;
 
-import com.example.buoi7.baikt.entity.Book;
+import com.example.buoi7.baikt.dto.request.BookCreateRequest;
+import com.example.buoi7.baikt.dto.request.BookUpdateRequest;
 import com.example.buoi7.baikt.entity.Author;
+import com.example.buoi7.baikt.entity.Book;
 import com.example.buoi7.baikt.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/book")
@@ -22,9 +22,24 @@ List<Book> getBook(@RequestParam long id){
     return bookService.getBook(id);
 }
 
+@PostMapping("/create")
+    public Book cretaeBook(@RequestBody BookCreateRequest request){
+    return bookService.createBook(request);
+}
 
+@PutMapping("{/id}")
+    public Book updateBook(@RequestBody BookUpdateRequest request, @RequestParam long id){
+    return bookService.updateBook(request,id);
+}
 
+@DeleteMapping("{/id}")
+    public String deleteBook(@RequestParam long id){
+    bookService.deleteBook(id);
+    return "thanh coong!!";
+}
 
-
-
+    @GetMapping("/authorId")
+    public Optional<Book> getByBook(@RequestParam("bookId") long bookId) {
+        return Optional.ofNullable(bookService.getBookId(bookId));
+    }
 }
